7293910b4546c7d9627cd9cc1a11c41f37d11f66
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-03-04 13:40:05 +0530

    Gallery page functionalities completed

---
+++ public/images_data.js
@@ -0,0 +1,53 @@
+//Comtains images data with tag
+const imageList = [
+  {
+    tag: "Workshop",
+    path: "../public/images/gallery/1.jpg",
+  },
+  {
+    tag: "AI Catechism",
+    path: "../public/images/gallery/5.jpg",
+  },
+  {
+    tag: "Workshop",
+    path: "../public/images/gallery/2.jpg",
+  },
+  {
+    tag: "Workshop",
+    path: "../public/images/gallery/3.jpg",
+  },
+  {
+    tag: "Prize Distribution",
+    path: "../public/images/gallery/11.jpg",
+  },
+  {
+    tag: "Prize Distribution",
+    path: "../public/images/gallery/12.jpg",
+  },
+  {
+    tag: "Meteor",
+    path: "../public/images/gallery/8.jpg",
+  },
+  {
+    tag: "Meteor",
+    path: "../public/images/gallery/9.jpg",
+  },
+  {
+    tag: "AI Lang Pro'19",
+    path: "../public/images/gallery/7.jpg",
+  },
+  {
+    tag: "AI Lang Pro'19",
+    path: "../public/images/gallery/10.jpg",
+  },
+  {
+    tag: "Innopreneurs",
+    path: "../public/images/gallery/4.jpg",
+  },
+  {
+    tag: "Innopreneurs",
+    path: "../public/images/gallery/6.jpg",
+  },
+];
+
+export default imageList;

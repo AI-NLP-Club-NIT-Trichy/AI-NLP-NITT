ee549782f6788070f209f6eb497f4f8410bbcd16
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-03-19 21:42:11 +0530

    profileCard changed

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,22 +2,23 @@
 import { FaLinkedin } from "react-icons/fa";
 
 const ProfileCard = ({ details }) => {
-  const { name, position,batch, linkedin, imagePath } = details;
+  const { name, position, batch, linkedin, imagePath } = details;
 
   return (
-    <div className="group relative w-60 rounded-xl border hover:scale-105 overflow-hidden hover:duration-300 shadow-lg group:">
+    <div className="group relative w-60 rounded-xl  hover:scale-105 overflow-hidden hover:duration-300 shadow-lg group:">
       <img className="w-full" src={imagePath} alt={`${name}'s profile`} />
-      <div className="absolute  collapse group-hover:visible group-hover:duration-500 group-hover:transition-all  p-4 inset-0 flex flex-col justify-center gap-5 items-center bg-black bg-opacity-50 text-white">
-        <h2 className="text-3xl bold font-semibold">{name}</h2>
-        {/* <p className="text-sm">{position}</p> */}
+
+      <div className="absolute  collapse group-hover:visible group-hover:duration-500 group-hover:transition-all  p-4 inset-0 flex flex-col justify-end gap-5 items-center bg-black bg-opacity-50 text-white">
         <a
           href={linkedin}
           target="_blank"
           rel="noopener noreferrer"
-            className="mt-2 text-4xl text-gray-300"
+          className="mt-2 text-3xl text-gray-300"
         >
           <FaLinkedin />
         </a>
+        <h2 className="text-2xl text-center bold font-semibold">{name}</h2>
+        {/* <p className="text-sm">{position}</p> */}
       </div>
     </div>
   );

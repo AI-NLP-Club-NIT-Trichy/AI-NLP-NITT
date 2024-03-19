f249da40c663a06f1ee4fc8ff0b1b473a8369fb1
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-03-19 18:13:02 +0530

    Teams Page Completed

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,38 +1,24 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { FaLinkedin } from "react-icons/fa";
 
 const ProfileCard = ({ details }) => {
-  const { name, position, linkedin, imagePath } = details;
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  const { name, position,batch, linkedin, imagePath } = details;
 
   return (
-    <div
-      className="relative w-64 rounded-xl overflow-hidden shadow-lg"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className="group relative w-60 rounded-xl border hover:scale-105 overflow-hidden hover:duration-300 shadow-lg group:">
       <img className="w-full" src={imagePath} alt={`${name}'s profile`} />
-      {isHovered && (
-        <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white">
-          <h2 className="text-xl font-semibold">{name}</h2>
-          <p className="text-sm">{position}</p>
-          <a
-            href={linkedin}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-2 underline"
-          >
-            LinkedIn Profile
-          </a>
-        </div>
-      )}
+      <div className="absolute  collapse group-hover:visible group-hover:duration-500 group-hover:transition-all  p-4 inset-0 flex flex-col justify-center gap-5 items-center bg-black bg-opacity-50 text-white">
+        <h2 className="text-3xl bold font-semibold">{name}</h2>
+        {/* <p className="text-sm">{position}</p> */}
+        <a
+          href={linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+            className="mt-2 text-4xl text-gray-300"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </div>
   );
 };

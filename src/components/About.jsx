624722d78c191f9545455111bfb60ce0990ed2e9
205123064 <205123064@nitt.edu>
2024-03-26 21:43:16 +0530

    changes to responsiveness added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,8 +17,7 @@ const Faculty = (props) => {
         <div className='md:w-10/12 mb-24 mt-10'>
           <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.4, duration: 0.3 }} viewport={{ once: false }} className="text-[20px] md:text-2xl mb-7 font-medium text-white js-build-in-item build-in-slideX-left build-in-animate" style={{ transitionDelay: '200ms' }}>FACULTY ADVISOR</motion.div>
           <motion.h3 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.5, duration: 0.3 }} viewport={{ once: false }} className="text-[28px] md:text-[40px] max-md:leading-8 max-lg:leading-10 lg:text-5xl mb-7 font-medium text-white js-build-in-item build-in-slideX-left build-in-animate" style={{ transitionDelay: '300ms' }}>
-            <span className="text-[#7ee787]">Introducing The Cornerstone of our Club, our Esteemed Faculty Advisor. </span><br />
-            Dr.(Mrs.) S. Sangeetha
+            <span className='text-[#7ee787]'>Dr.(Mrs.) S. Sangeetha</span>
           </motion.h3>
         </div>
       </div>

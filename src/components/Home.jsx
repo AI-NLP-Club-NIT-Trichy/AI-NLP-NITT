1d366a3684ae04b24571ba863c6bf9a433571dc5
Prasanjeet-Karmakar <prasan.9120@gmail.com>
2024-04-24 00:29:32 +0530

    carousel added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
-import Carousel from "./background/Carousel";
+import Carousel from "./Carousel/Carousel";
 import GlyphButton from "./glyph_button/GlyphButton";
+
 const Home = () => {
   return (
-    <div className="relative w-full h-[calc(100vh-80px-67.2px)]  sm:h-[calc(100vh-80px-90px)] overflow-hidden bg-black">
+    <div
+      style={{
+        backgroundImage:
+          'url("https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/img/demo-1-bg.jpg")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="relative w-full h-[calc(100vh-80px-67.2px)]  sm:h-[calc(100vh-80px-90px)] overflow-hidden bg-black"
+    >
       <div className="text-center w-full h-full text-7xl absolute top-0 left-0 z-10 flex flex-col items-center justify-evenly">
         <div>
           <GlyphButton text={"AI & NLP"} speed={0.4} />

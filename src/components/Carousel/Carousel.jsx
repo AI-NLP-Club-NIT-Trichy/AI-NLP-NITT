f215cbd879f0f2ecfb91f6ce936bfac3b60719db
P_Janardhan <pjanardhan7u@gmail.com>
2024-04-26 17:21:34 +0530

    home page corousel made responsive

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,45 +3,37 @@ import { Carousel } from "antd";
 const elements = [
   {
     src: "/images/gallery/ai_lang_pro_2024/16.jpeg",
-    descripton: "Snap of AI Lang Pro Event 2024: Financial Relation Prediction",
+    description: "Snap of AI Lang Pro Event 2024: Financial Relation Prediction",
   },
   {
     src: "/images/gallery/ai_lang_pro_2024/15.jpeg",
-    descripton: "AI Lang Pro 24 1st Prize",
+    description: "AI Lang Pro 24 1st Prize",
   },
   {
     src: "/images/gallery/ai_lang_pro_2024/13.jpeg",
-    descripton: "AI Lang Pro 24 2nd Prize",
+    description: "AI Lang Pro 24 2nd Prize",
   },
   {
     src: "/images/gallery/ai_lang_pro_2024/14.jpeg",
-    descripton: "AI Lang Pro 24 3rd Prize",
+    description: "AI Lang Pro 24 3rd Prize",
   },
 ];
 
-const carouselElements = elements.map((element) => {
-  return (
-    <div>
-
-      <img src={element.src} className="h-[99%] w-full object-cover" />
-      <p className="text-white mt-2">{element.descripton}</p>
-
-    </div>
-  );
-});
+const carouselElements = elements.map((element, index) => (
+  <div key={index} className="w-full h-full flex flex-col justify-center items-center  rounded-xl">
+    <img src={element.src} className="max-h-[70vh] w-full object-contain rounded-xl" alt={element.description} />
+    <p className="text-white font-bold sm:text-xl mt-2">{element.description}</p>
+  </div>
+));
 
 const CarouselElement = () => {
   return (
-    <div>
-      <Carousel
-        autoplay
-
-        className="flex flex-col h-[34vh]   sm:h-[75vh] sm:max-h-[60vhpx] w-[90vw] sm:max-w-[70vw] border-4 p-2 rounded-xl border-white"
-
-      >
+    <div className="mx-auto max-w-[90vw] sm:max-w-[70vw]">
+      <Carousel autoplay className="h-[70vh] sm:h-[50vh] lg:h-[70vh]">
         {carouselElements}
       </Carousel>
     </div>
   );
 };
+
 export default CarouselElement;
